da5613126e4ac76f3fb4a0649917cc6d0250668f
VestaxUA <valiktkachishen@gmail.com>
2023-09-22 17:38:08 +0300

    Making a core for book list

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,15 @@
 package org.example;
 
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Book book1 = new Book();
+
+        Library library = new Library();
+
+        library.addBook(book1);
+        
     }
 }
